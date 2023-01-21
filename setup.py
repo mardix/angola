9ff723beb27769b247982a30aba702507d50c668
Mardix <mardix@iamblackdev.io>
2023-01-21 15:26:53 -0500

    v 5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def long_description():
     with open('README.md', 'r') as file:
         return file.read()
 
-VERSION = "0.4.1"
+VERSION = "0.5.0"
 setuptools.setup(
     name='angola',
     version=VERSION,
