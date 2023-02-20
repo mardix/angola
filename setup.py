adff23be99c8546c6c3192b609207dd4b06b8740
Mardix <mardix@iamblackdev.io>
2023-02-20 17:06:11 -0500

    add delete() method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def long_description():
     with open('README.md', 'r') as file:
         return file.read()
 
-VERSION = "0.10.4"
+VERSION = "0.10.5"
 setuptools.setup(
     name='angola',
     version=VERSION,
